e303c7b9039b642bacc740fa567222936b66f957
andgom97 <andgomalf@gmail.com>
2020-12-10 19:18:49 +0100

    dish_info layout 1st draft

---
+++ app/src/main/java/com/example/dishapp/DishCreation.java
@@ -0,0 +1,15 @@
+package com.example.dishapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DishCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dish_creation);
+    }
+}
+
