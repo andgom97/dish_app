cd9f04537421e923001498fbe3ca84ee61910a72
andgom97 <andgomalf@gmail.com>
2020-12-13 20:15:35 +0100

    dish_creation spinner instead of radio buttons

--- app/src/main/java/com/example/dishapp/DishCreation.java
+++ app/src/main/java/com/example/dishapp/DishCreation.java
@@ -3,6 +3,14 @@ package com.example.dishapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DishCreation extends AppCompatActivity {
 
@@ -10,6 +18,34 @@ public class DishCreation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dish_creation);
+
+        // Spinner element
+        Spinner moment_spinner = (Spinner) findViewById(R.id.moment_spinner);
+        Spinner diet_spinner = (Spinner) findViewById(R.id.diet_spinner);
+
+        // Creating adapters
+        ArrayAdapter<CharSequence> moment_adapter = ArrayAdapter.createFromResource(this,R.array.moments,android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> diet_adapter = ArrayAdapter.createFromResource(this,R.array.diets,android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        moment_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        diet_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        moment_spinner.setAdapter(moment_adapter);
+        diet_spinner.setAdapter(diet_adapter);
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }
 
